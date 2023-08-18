0d74ebf14d3b361146e97ae49afae22aee3f7674
fellly <fellyka@gmail.com>
2023-08-18 12:05:49 +0200

    Product service added

--- ShopOnline.Web/Program.cs
+++ ShopOnline.Web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Point to the webapi project - Use this during dev
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7179/") });
 
 await builder.Build().RunAsync();
---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;  
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                //Get the value from an Uri and deserialize it in Json
+                var products = await this.httpClient.GetFromJsonAsync<IEnumerator<ProductDto>>("api/Product");
+                return (IEnumerable<ProductDto>)products;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
